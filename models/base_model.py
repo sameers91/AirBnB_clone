d708328b2d9d24f40facb2a0c88e8cb39d02930d
NiteshHalai <nitesh.halai@gmail.com>
2023-02-08 23:12:26 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,6 +13,14 @@ class BaseModel():
     """
 
     def __init__(self):
+        
+        if kwargs != {}:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+        
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
 
