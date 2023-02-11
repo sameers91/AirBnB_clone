de0375436d927c7a4ed27ab499b6f686859130ad
NiteshHalai <nitesh.halai@gmail.com>
2023-02-11 17:52:34 +0300

    Update state.py

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+'''
+class State that inherits from BaseModel
+'''
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    This is a class
+    """
+
+    name = ''
