b6adeb60ec9e0d3b849aafffa85294f25acb514c
NiteshHalai <nitesh.halai@gmail.com>
2023-02-11 17:51:33 +0300

    Update city.py

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+class city that inherits from BaseModel
+'''
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """
+    This is a class
+    """
+    
+    state_id =''
+    name = ''
+    
