72e838aeed2e0459d7f22296c8af6b1c8dbb3bf8
NiteshHalai <nitesh.halai@gmail.com>
2023-02-11 20:56:03 +0300

    Update console.py

--- console.py
+++ console.py
@@ -36,20 +36,39 @@ class HBNBCommand(cmd.Cmd):
         print ("syntax: EOF")
         print ("-- terminates the application")
 
-    def do_create(self, args):
-        
-        if len(args) == 0:
-            print("** class name missing **")
-        else:
-            args_list = args.split()
-            if args_list[0] not in ['BaseModel']:
-                print(args_list[0])
-                print('** class name missing **')
+    def do_create(self, line):
+        """Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with given keys/values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError()
+            my_list = line.split(" ")
+
+            kwargs = {}
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        continue
+                kwargs[key] = value
+
+            if kwargs == {}:
+                obj = eval(my_list[0])()
             else:
-                if args_list[0] == 'BaseModel':
-                    my_model = BaseModel()
-                    my_model.save()
-                    print (my_model.id)
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            print(obj.id)
+            obj.save()
+
+        except SyntaxError:
+            print("** class name missing **")
+        except NameError:
+            print("** class doesn't exist **")
 
     def help_create(self):
         print ("syntax: create")
