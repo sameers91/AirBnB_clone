8639c1c01af579be69c4ea97f37111ea440efd24
NiteshHalai <nitesh.halai@gmail.com>
2023-02-11 17:50:30 +0300

    Update amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+class Amenity that inherits from BaseModel
+'''
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    This is a class
+    """
+
+    name = ''
+    
