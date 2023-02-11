548cb5f336f397be4e6dca3c1b7cd521af8aaca6
NiteshHalai <nitesh.halai@gmail.com>
2023-02-11 14:40:15 +0300

    Update user.py

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''
+class User that inherits from BaseModel
+'''
+
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    This is a class
+    """
+
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
